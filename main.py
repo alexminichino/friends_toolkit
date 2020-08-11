20fe25c505275afd88eb474916d87b9a7ca80e19
Alexander Minichino <alexminichino@gmail.com>
2020-08-11 19:21:42 +0200

    caching mechanism on friends list

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import datetime
 import webbrowser
 import math
 import sys
+import json
 from flask import Flask, render_template, redirect, request, session,flash, send_file
 from werkzeug.utils import secure_filename
 from messages_tool import *
 
 REPORT_FILE_NAME = 'contacted_users.csv'
 UPLOAD_DIRECTORY = 'uploads'
+FRIENDS_DUMP_FILE ="friends.dump"
 app = Flask(__name__)
 app.secret_key = "RANDOM_STRING".encode('utf8')
 webbrowser.open('http://localhost:5000', new=1)
@@ -57,7 +59,7 @@ def select_friends():
     if len(unselected_users)>0:
          selected_users = list(set(selected_users) - set(unselected_users))
     session['selected_users'] = selected_users 
-    friends = messages_tool.fetch_users()
+    friends = get_serialized_friends_or_fetch_all()
     number_friends = len(friends)
     friends = friends[offset:(limit + offset if limit is not None else None)]
     return render_template("friends.html", friends= friends , contacted_friends=  get_contacted_friends_by_csv(), report_exitst = report_exitst(), number_friends= number_friends, selected_users=selected_users, number_pages=math.ceil(number_friends/limit), current_page = page, limit_friends_per_page=limit)
@@ -74,7 +76,8 @@ def compose_message():
     if len(selected_users) < 1:
         flash("Select at least 1 friend!")
         return redirect("/select_friends") 
-    return render_template("compose_message.html", friends= [(user) for user in messages_tool.fetch_users() if user.uid in selected_users], old_images=old_images )
+    friends= get_serialized_friends_or_fetch_all()
+    return render_template("compose_message.html", friends= [(user) for user in friends if user.uid in selected_users], old_images=old_images )
 
 @app.route("/send_messages",methods=["POST"])
 def send_messages():    
@@ -99,7 +102,7 @@ def send_messages():
     else:
         return redirect("/select_friends")
     message_backup= message
-    for user in messages_tool.fetch_users():
+    for user in get_serialized_friends_or_fetch_all():
         if user.uid in selected_users:
             message=message_backup
             message= message.replace("{{name}}",user.name)
@@ -119,6 +122,7 @@ def send_messages():
 @app.route("/logout")
 def logout():
     session.clear()
+    os.remove(FRIENDS_DUMP_FILE)
     return redirect("/sign_in")
 
 @app.route("/report")
@@ -175,6 +179,36 @@ def get_contacted_friends_by_csv():
 
 def report_exitst():
     return os.path.exists(REPORT_FILE_NAME)
+
+def get_serialized_friends_or_fetch_all():
+    if os.path.exists(FRIENDS_DUMP_FILE):
+        return read_friends_from_dump_file()
+    friends=[]
+    for friend in messages_tool.fetch_users():
+        if friend.is_friend:
+            friends.append(
+                SimplifiedUser(
+                        uid=friend.uid,
+                        name=friend.name,
+                        first_name = friend.first_name,
+                        gender = friend.gender,
+                        photo = friend.photo,
+                        url = friend.url
+                    ))
+    json_values= json.dumps(friends, default=convert_to_dict) 
+                  
+    write_friends_on_dump_file(json_values)
+    return friends
+
+def read_friends_from_dump_file():
+     f = open(FRIENDS_DUMP_FILE, "r")
+     with f:
+         return json.loads(f.read(), object_hook=dict_to_obj)
+
+def write_friends_on_dump_file(json_values):
+     f = open(FRIENDS_DUMP_FILE, "w")
+     with f:
+         f.write(json_values)
 
 if __name__ == "__main__":
     app.run(debug=False)
--- messages_tool.py
+++ messages_tool.py
@@ -1,9 +1,12 @@
+import logging
+import json
 from fbchat import Client
 from fbchat.models import *
 
+
 class MessagesTool(object):
     
-    def __init__(self,email="fakeemail@gmail.com", password ="fakePassword"): #Fake credentials are needed in case of authentication by Session Cookie  
+    def __init__(self,email="fakeemail@gmail.com", password ="fakePassword", logging_level=logging.DEBUG): #Fake credentials are needed in case of authentication by Session Cookie  
        self.authenticated = False
        self.email= email
        self.password= password       
@@ -50,6 +53,54 @@ class MyClient(Client):
     def on2FACode(self):
         """Called when a 2FA code is needed to progress."""
         return input("Please enter your 2FA code -->")
+
+class SimplifiedUser():
+    def __init__(self, uid, name, first_name, gender, photo, url ):
+        self.uid=uid
+        self.name=name
+        self.first_name = first_name
+        self.gender = gender
+        self.photo = photo
+        self.url = url
     
+def convert_to_dict(obj):
+    """
+    A function takes in a custom object and returns a dictionary representation of the object.
+    This dict representation includes meta data such as the object's module and class names.
+    """
+    
+    #  Populate the dictionary with object meta data 
+    obj_dict = {
+        "__class__": obj.__class__.__name__,
+        "__module__": obj.__module__
+    }
+    
+    #  Populate the dictionary with object properties
+    obj_dict.update(obj.__dict__)
+    
+    return obj_dict
 
-
+def dict_to_obj(our_dict):
+    """
+    Function that takes in a dict and returns a custom object associated with the dict.
+    This function makes use of the "__module__" and "__class__" metadata in the dictionary
+    to know which object type to create.
+    """
+    if "__class__" in our_dict:
+        # Pop ensures we remove metadata from the dict to leave only the instance arguments
+        class_name = our_dict.pop("__class__")
+        
+        # Get the module name from the dict and import it
+        module_name = our_dict.pop("__module__")
+        
+        # We use the built in __import__ function since the module name is not yet known at runtime
+        module = __import__(module_name)
+        
+        # Get the class from the module
+        class_ = getattr(module,class_name)
+        
+        # Use dictionary unpacking to initialize the object
+        obj = class_(**our_dict)
+    else:
+        obj = our_dict
+    return obj
